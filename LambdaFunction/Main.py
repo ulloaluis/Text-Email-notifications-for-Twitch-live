d3c09d804b3e424266d4ba62e31ccb339249ccc6
Luis Ulloa <scssaluis@gmail.com>
2018-07-26 15:51:45 -0700

    name task

--- LambdaFunction/Main.py
+++ LambdaFunction/Main.py
@@ -10,6 +10,10 @@ def response(message, code):
         'body': message
     }
 
+# "thumbnail_url":"...live_user_<username>-{width}x{height}.jpg"
+def parse_name(body):
+    return body['data'][0]['thumbnail_url'].split("user_")[1].split("-{width}")[0]
+
 # Callback handler that receives either 1) a challenge for verifying
 # a webhook subsciption or 2) a notification payload with details about
 # a streamer going on/off-line, in which case we write to message.txt and
@@ -24,10 +28,11 @@ def lambda_handler(event, context):
     elif 'body' in event and event['body']: # body exists and is not None
         body = json.loads(event['body']) # currently a string, load as json
         if len(body.get('data')) > 0: # data is well-formed (would be size 1 with {data here})
+            username = parse_name(body)
+            start_time = body['data'][0]['started_at']
+            stream_title = body['data'][0]['title']
             with open('/tmp/message.txt', 'w') as msg: # update message file
-                start_time = body['data'][0]['started_at']
-                stream_title = body['data'][0]['title']
-                msg.write("Now streaming since %s: %s" % (start_time, stream_title))
+                msg.write("%s is now streaming since %s: %s" % (username, start_time, stream_title))
             with open('/'.join([os.path.dirname(__file__), 'data.json']), 'r') as f: # send email/text
                 data = json.load(f) # json file must be in same directory!
                 send_email(data['Email'])
